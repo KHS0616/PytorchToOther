1aa8fd4a7e56f0352df7b8a8ebabdcb7b3258575
KHS0616 <doctorsol@naver.com>
2021-06-08 09:34:27 +0900

    Ver 1.0.1
    1. TFLite 양자화 기법 추가

--- Maker/tensorlite_maker.py
+++ Maker/tensorlite_maker.py
@@ -2,7 +2,7 @@
 Tensorflow 모델을 TensorLite 모델로 변환하는 모듈
 
 Writer : KHS0616
-Last Update : 2021-05-27
+Last Update : 2021-06-03
 """
 # 경로 및 시스템 관련 모듈
 import os
@@ -11,6 +11,9 @@ import os
 # Tensorflow & TensorLite 관련 모듈
 import tensorflow as tf
 from Maker.tensorflow_maker import Main as TM
+
+import cv2
+import numpy as np
 
 class Main():
     """ 메인 클래스 """
@@ -34,10 +37,38 @@ class Main():
         pb_path = os.path.join(os.path.join(self.opt["root"]["tensorflow"], self.opt["tensorflow"]["path"]))
         converter = tf.lite.TFLiteConverter.from_saved_model(pb_path)
 
+        # 에러 방지
         converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
                                             tf.lite.OpsSet.SELECT_TF_OPS]
 
-        # converter.optimizations = [tf.compat.v1.lite.Optimize.DEFAULT]
+        if self.opt["tensorlite"]["int8_full"]:
+            converter.optimizations = [tf.lite.Optimize.DEFAULT]
+            # converter.target_spec.supported_types = [tf.float16]
+            converter.representative_dataset = self.representative_data_gen
+            converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+            converter.inference_input_type = tf.uint8
+            converter.inference_output_type = tf.uint8
 
         tf_lite_model = converter.convert()
         open(os.path.join(self.opt["root"]["tensorlite"], self.opt["tensorlite"]["path"]), "wb").write(tf_lite_model)
+
+    def makeCalibrationDataset(self):
+        """ Calibration 데이터 셋 생성 메소드 """
+        self.cali_datasets = []
+        for v in os.listdir(self.opt["tensorlite"]["cali_path"]):
+            img = cv2.imread(os.path.join(self.opt["tensorlite"]["cali_path"], v)).astype(np.float32)
+            # img = cv2.resize(img, (960,540), cv2.INTER_CUBIC)
+            img = cv2.resize(img, (40,40), cv2.INTER_CUBIC)
+            img = img.transpose(2, 0, 1) / 255.
+            # img = tf.io.read_file(os.path.join(self.opt["tensorlite"]["cali_path"], v))
+            # img = tf.image.decode_jpeg(img)
+            # img = tf.image.random_crop(img, [128, 128, 3], seed=None, name=None)
+            # img = tf.cast(img, tf.float32) / 255.0
+            self.cali_datasets.append(img)
+
+    def representative_data_gen(self):
+        """ TensorLite 버전 Calibration 메소드 """ 
+        self.makeCalibrationDataset()
+        for input_value in tf.data.Dataset.from_tensor_slices(self.cali_datasets).batch(1).take(100):
+            # Model has only one input so each data point has one element.
+            yield [input_value]
